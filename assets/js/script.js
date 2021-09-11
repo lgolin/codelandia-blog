c1ccd72f73dec6bcca3de0f41b67908ee3dbdd80
Lais <laisgolin@gmail.com>
2021-09-11 09:36:35 +0100

    add search & likes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 // Like
-const likes = document.querySelectorAll(".like");
-
-for (let like of likes) {
-  like.addEventListener("click", () => liked(like));
+const likes = document.querySelectorAll('.like');
+for(let like of likes) {
+  like.addEventListener("click", () => liked(like))
 }
 
-const liked = (item) => {
-  if (item.classList.contains("far")) {
+const liked = item => {
+  if(item.classList.contains("far")) {
     item.classList.remove("far");
-    item.classList.add("fas");
+    item.classList.add("fas")
   } else {
     item.classList.remove("fas");
-    item.classList.add("far");
+    item.classList.add("far")
   }
-};
+}
 
 // Search
 const input = document.getElementById("search-bar");
